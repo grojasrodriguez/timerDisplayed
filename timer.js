ae48fc46c31503851caa53492daa62289ee4ded4
grojasrodriguez <78385381+grojasrodriguez@users.noreply.github.com>
2021-05-07 17:49:01 -0400

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,27 @@
+var c = 0;
+var t;
+var timer_is_on = 0;
+
+function timedCount() {
+  document.getElementById("second").innerHTML = c;
+  c = c + 1;
+  t = setTimeout(timedCount, 1000);
+}
+
+function startTimer() { 
+  if (!timer_is_on) {
+    timer_is_on = 1;
+    timedCount();
+  }
+}
+
+function stopTimer() {
+  clearTimeout(t);
+  timer_is_on = 0;
+}
+
+function resetTimer() {
+    stopTimer();
+    c=0;
+    startTimer();
+}
